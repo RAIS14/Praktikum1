d806318c659c97825237e6f7862cc8dddc9f8767
rais <raismuf@gmail.com>
2018-11-11 07:22:38 +0700

    menentukan bilangan genap/ganjil

---
+++ latihan1.1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+  cout << "Masukan Bilangan :"<< endl;
+  cin>>x;
+    if (x%2==0)
+    cout<<" Bilangan yang di masukkan adalah bilangan Genap";
+    else
+    cout<<"Bilangan yang di masukan adalah bilangan Ganjil";
+    return 0;
+}
